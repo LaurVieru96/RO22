51cdd4e4cddd7262c0a853cccb1139d0b5720f4f
LaurentiuVI <laurentiuvieru50@gmail.com>
2022-09-01 13:07:47 +0300

    New bootstrap project + assets & onclick function

---
+++ bootstrapProject/script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('button');
+
+button.addEventListener('click', function onclick() {
+    button.style.backgroundColor = 'green';
+    button.style.color = 'black';
+    button.style.fontWeight = '700';
+    
+});
